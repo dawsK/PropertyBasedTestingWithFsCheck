a247967a65431c42feea1e57d4d7f2346501aede
iqobsdawsonk <dawsonk@iqmetrix.com>
2016-03-20 21:43:33 -0500

    updates

--- RomanNumeralsFSharp/Library1.fs
+++ RomanNumeralsFSharp/Library1.fs
@@ -1,29 +1,58 @@
 ﻿namespace RomanNumeralsFSharp
 
+open System
 open Xunit
 open FsUnit.Xunit
-open FsCheck
 open FsCheck
 open FsCheck.Xunit
 open RomanNumeralsCSharp
 
 module Tests =
 
-    let numerals = [|'M', 'D', 'C', 'L', 'X', 'V', 'I'|]    
-
+    
 
     type RomanNumerals =
-        static member ValidInteger = Arb.Default.Int32
+        static member ValidInteger = 
+            Arb.Default.Int32() 
+            |> Arb.filter (fun i -> i >= 1)
+            |> Arb.filter (fun i -> i <= 3999)
 
+    type RomanNumeralProperty () =
+        inherit PropertyAttribute(Arbitrary = [|typeof<RomanNumerals>|])
 
-
-    [<Property>]
+    //[<Property(Arbitrary = [|typeof<RomanNumerals>|])>]
+    [<RomanNumeralProperty>]
     let ``To Rome and back gives the same result`` number =
         let result = 
             number 
             |> RomanNumerals.ToRoman 
             |> RomanNumerals.ToInteger
         result = number
+
+    [<RomanNumeralProperty>]
+    let ``Always uses valid characters`` number =
+        let isRomanNumeral c = "MDCLXVI" |> String.exists (fun x -> x = c)
+
+        let result = number |> RomanNumerals.ToRoman
+        result |> String.forall isRomanNumeral
+
+    [<RomanNumeralProperty>]
+    let ``Never returns empty string`` number =
+        let result = number |> RomanNumerals.ToRoman
+        (result |> String.length) > 0
+
+    let hasMaxRepeats x c (str:String) = 
+        not (str.Contains(String.replicate (x + 1) c))
+
+    [<RomanNumeralProperty>]
+    let ``Repeats 'I' a maximum of 3 times`` number =        
+        let result = number |> RomanNumerals.ToRoman
+        result |> hasMaxRepeats 3 "I"
+
+    [<RomanNumeralProperty>]
+    let ``Repeats 'V' a maximum of 1 time`` number =
+        let result = number |> RomanNumerals.ToRoman
+        result |> hasMaxRepeats 1 "V"
 
 //    [<Property>]
 //    let ``Ends with a max of 3 I's`` number =
@@ -34,7 +63,7 @@ module Tests =
 //            |> List.rev
 //            |> Seq.takeWhile (fun c -> c = 'I')
 //            |> Seq.length
-////        numTrailingIs = 3
+//        numTrailingIs = 3
 
 //    [<Property>]
 //    let ``Roman numerals are always descending`` number =
