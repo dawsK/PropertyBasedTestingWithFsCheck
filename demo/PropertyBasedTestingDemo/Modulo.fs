5ea8c87678fd0ab2b053dfce310626cb0ba239fc
Dawson Kroeker <dkroeker@gmail.com>
2016-03-22 01:20:55 -0500

    Modulo Demo - Finish

--- demo/PropertyBasedTestingDemo/Modulo.fs
+++ demo/PropertyBasedTestingDemo/Modulo.fs
@@ -31,4 +31,38 @@ module PropertyTests =
     open FsCheck
     open FsCheck.Xunit
 
+    let modulo (a : bigint) (n : bigint) =
+        match a % n with
+        | x when x < 0I -> 
+            x + bigint.Abs(n)
+        | x -> x
     
+    [<Property>]
+    let ``modulo 1 always returns 0`` a =
+        modulo a 1I = 0I
+
+    [<Property>]
+    let ``a modulo n is always < |n|`` a n =
+        (n <> 0I) ==> lazy(modulo a n < abs(n))
+
+    [<Property>]
+    let ``identity: (a mod n) mod n = a mod n`` a n =
+        (n <> 0I) ==> lazy(modulo (modulo a n) n = modulo a n)
+
+    [<Property>]
+    let ``identity: n^x mod n = 0, x > 0`` n x =
+        (x > 0 && n <> 0I) ==> lazy(modulo (pown n x) n = 0I)
+
+
+    [<Property>]
+    let ``inverse: [(−a mod n) + (a mod n)] mod n = 0`` a n =
+        n <> 0I ==> lazy(modulo ((modulo -a n) + (modulo a n)) n = 0I)
+    
+    [<Property>]
+    let ``distributive: (a + b) mod n = [(a mod n) + (b mod n)] mod n`` a b n =
+        (n <> 0I) ==> lazy((modulo (a + b) n) = (modulo ((modulo a n) + (modulo b n)) n))
+    
+    [<Property>]
+    let ``distributive: ab mod n = [(a mod n)(b mod n)] mod n`` a b n =
+        (n <> 0I) ==> lazy((modulo (a * b) n) = (modulo ((modulo a n) * (modulo b n)) n))
+
